0562a50696f7b8fc1dcd7e8806b60bb01b6a60a7
vishalvaghani <vishalvaghani1434@gmail.com>
2024-05-14 15:25:41 +0530

    First commit

--- src/ReactRedux/Components/HeaderComponent.js
+++ src/ReactRedux/Components/HeaderComponent.js
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CartImg from '../../../src/cart.png'
 import { Table, Button, Modal, Container, Row, Col, Form } from 'react-bootstrap';
+import axios from 'axios';
 
 function HeaderComponent(props) {
     debugger
     var uniqueProduct=[];
+    const [data,setData]=useState([]);
     props.data.obj1.map(item=>{
         if(uniqueProduct.indexOf(item.id) === -1){
             uniqueProduct.push(item.id)
         }
     })
 
+    useEffect(()=>{
+        fetchAllStudent();
+    },[])
+
+    const fetchAllStudent = async () => {
+        try {
+            const res = await axios.get("https://web-backend-api-575c4fb7fce0.herokuapp.com/student");
+            console.log(res);
+            setData(res.data);
+        } catch (err) {
+            console.log(err);
+
+        }
+    };
 
     return (
         <React.Fragment>
+
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>College</th>
+                <th>Subject</th>
+            </tr>
+            {
+                data.map((item)=>
+                    <tr><td>{item.id}</td>
+                        <td>{item.name}</td>
+                        <td>{item.gender}</td>
+                        <td>{item.college}</td>
+                        <td>{item.subject}</td>
+                        <td>{item.stream}</td>
+                    </tr>
+                )
+            }
+        </table>
         <div className='add-to-cart'>
             <span className='cart-count'>{uniqueProduct.length}</span>
             <img src={CartImg} />
