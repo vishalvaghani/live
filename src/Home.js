736e5fa3069dbcd71136a9bc5b7a2cd14e36eed1
vishalvaghani <vishalvaghani1434@gmail.com>
2024-05-17 15:57:39 +0530

    make it better

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Container } from "react-bootstrap";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
     const navigate = useNavigate('');
